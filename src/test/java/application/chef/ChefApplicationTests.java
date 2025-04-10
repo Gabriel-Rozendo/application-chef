92566cf87050cb78ca579f54d6c4123d6beafa01
Gabriel <gabrielrozendodacosta@gmail.com>
2025-04-10 20:14:59 -0300

    first commit

---
+++ src/test/java/application/chef/ChefApplicationTests.java
@@ -0,0 +1,13 @@
+package application.chef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
